1078c44cc33951db6d4ae74ba928d481458a6065
risaannisa <rzkans001@gmail.com>
2026-05-13 11:32:59 +0700

    menambahkan plot

--- coba.R
+++ coba.R
@@ -2,4 +2,9 @@ summary(mtcars)
 mean(mtcars$mpg)
 min(mtcars$mpg)
 
-#analisis regresi 
+#Membuat plot antara konsumsi bbm (mpg) dan berat mobil (wt)
+plot(mtcars$wt, mtcars$mpg, 
+     main="Hubungan Berat Mobil vs MPG",
+     xlab="Berat Mobil", 
+     ylab="Miles Per Gallon", 
+     pch=19, col="maroon")
